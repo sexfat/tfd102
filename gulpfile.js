f3499cc13923ac374347bdddd16976734ac2c47e
sexfat <bryant6831@gmail.com>
2021-07-19 11:00:46 +0800

    add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const {
- src , dest , series , parallel , watch
+    src, dest, series, parallel, watch
 } = require('gulp');
 
 // 任務一
-function defaultask(cb){
-   console.log('hi gulp4');
-   cb();
+function defaultask(cb) {
+    console.log('hi gulp4');
+    cb();
 }
 
 // 任務輸出
@@ -16,40 +16,65 @@ exports.do = defaultask;
 
 
 //任務Ａ
-function missionA(cb){
-   console.log('missionA');
-   cb();
+function missionA(cb) {
+    console.log('missionA');
+    cb();
 }
 
 
 //任務Ｂ
-function missionB(cb){
-   console.log('missionB');
-   cb();
+function missionB(cb) {
+    console.log('missionB');
+    cb();
 }
 
 //任務C
-function missionC(cb){
-   console.log('missionC');
-   cb();
+function missionC(cb) {
+    console.log('missionC');
+    cb();
 }
 
 //任務D
-function missionD(cb){
-   console.log('missionD');
-   cb();
+function missionD(cb) {
+    console.log('missionD');
+    cb();
 }
 
 
 
 //series 順序 
-exports.async = series(missionA , missionB);
+exports.async = series(missionA, missionB);
 
-exports.tasks = series(missionA , missionB , parallel(missionC , missionD));
+exports.tasks = series(missionA, missionB, parallel(missionC, missionD));
 
 
 //  同時執行
-exports.sync = parallel(missionA , missionB);
+exports.sync = parallel(missionA, missionB);
+
+
+// uglify js
+
+const uglify = require('gulp-uglify');
+
+
+function ugjs() {
+    return src('js/scripts.js') //來源
+    .pipe(uglify()) //打包js
+    .pipe(dest('js/minify/')) // 目的地
+}
+
+
+exports.taskjs = ugjs 
+
+
+
+
+
+
+
+
+
+
 
 
 
