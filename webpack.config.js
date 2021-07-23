6d5f6a12d9af65914653d6ad098bbe56a0c048f5
sexfat <bryant6831@gmail.com>
2021-07-23 16:34:05 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -33,5 +33,5 @@ module.exports = {
         })
     ],           // 對應的插件
     //devServer: {},           // 服務器配置
-    //mode: 'development'      // 開發模式配置
+    mode: 'production'      // 開發模式配置 development  production
 }
