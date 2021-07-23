11d594f71f5eec40c49b22fc2708e5a0442f5177
sexfat <bryant6831@gmail.com>
2021-07-23 15:34:13 +0800

    add import plugin

--- src/app.js
+++ src/app.js
@@ -3,6 +3,26 @@ import './css/style.css';
 import './css/header.css';
 import './css/footer.css';
 
+// gsap 注入
+import { TweenMax } from 'gsap';
+
+// jquery
+import $ from 'jquery';
+
+
+
+$('body').css('background-color' , 'green');
+
+
+TweenMax.to('.box' , 1 , {
+ x: 400,
+ rotation : 360,
+ repeat: -1,
+ yoyo: true
+})
+
+
+
 
 const add = (a, b) => a * b ;
 console.log(add(100 , 20) + '像素'); 
