f8c54eca161a04391202e08a68a9f619733bf3b1
sexfat <bryant6831@gmail.com>
2021-07-26 15:27:36 +0800

    jquery an vue 共用

--- src/js/app.js
+++ src/js/app.js
@@ -5,24 +5,39 @@ import './../sass/style.scss';
 // gsap 注入
 import { TweenMax } from 'gsap';
 
+// vue 注入
+
+import Vue from "vue";
+
 // jquery
 // import $ from 'jquery';
 
 
+// jquery
+$('body').css('background-color', 'yellow');
 
-$('body').css('background-color' , 'green');
+
+// vue 
+new Vue({
+    el: '#app',
+    data: {
+        message: '首頁tfd102'
+    }
+
+})
 
 
-TweenMax.to('.box' , 1 , {
- x: 400,
- rotation : 360,
- repeat: -1,
- scale: 2,
- yoyo: true
+// tweenmax
+TweenMax.to('.box', 1, {
+    x: 400,
+    rotation: 360,
+    repeat: -1,
+    scale: 2,
+    yoyo: true
 })
 
 
 
 
-const add = (a, b) => a * b ;
-console.log(add(100 , 20) + '像素'); 
+const add = (a, b) => a * b;
+console.log(add(100, 20) + '像素');
